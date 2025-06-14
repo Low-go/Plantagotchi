08343cf0ab973916ede1496d394055f021c628f1
Low-go <lorranalvesgaldino98@gmail.com>
2025-06-13 15:22:09 -1000

    post method on plant works, receives plant dto, creates object, saves as json file

--- src/Plantagotchi/Controllers/PlantController.cs
+++ src/Plantagotchi/Controllers/PlantController.cs
@@ -2,32 +2,54 @@
 
 
 using System;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using Plantagotchi.Dto;
 using Plantagotchi.Models;
+using System.Text.Json;
+using System.IO;
 
 namespace Plantagotchi.Controllers
 {   // our top level route
     [ApiController]
     [Route("plant")]
 
-    public class PlantController
+    public class PlantController : ControllerBase
     {
 
-        [HttpGet("{id}")]
-        public Plant GetPlantById(Guid id)
-        {
-            
-        }
+        // [HttpGet("{id}")]
+        // public Plant GetPlantById(Guid id)
+        // {
 
+        // }
 
-        [HttpPost("{id}")] // i think??? do we need an id
-        public ActionResult<Plant> CreatePlant(Plant plant)
+        /*
+            * This is how it works. The thing making the call to the post request
+            makes a plant dto
+
+            * it oasses the plant dto into the post request
+
+            * the post request is responsible for instantiating the plant object
+
+            * it also saves info in json and returns plant object
+        */
+        [HttpPost]
+        public ActionResult<Plant> CreatePlant(CreatePlantDto plantDto)
         {
             // return CreatedAtActionResult(nameof(GetPlantById), new {id = plant.id}, plant);
 
-            Plant newPlant = new Plant();
-            return Ok(plant);
+            var newPlant = new Plant(plantDto.Name);
+            var jsonPayLoad = JsonSerializer.Serialize(newPlant);
+            Console.WriteLine(jsonPayLoad);
+
+
+            // NOTE TO SELF  this overwrites the whole json file
+            // if you decide to add more plants in the future you will
+            // have to deserialize this and store it
+            string path = "Data/plants.json";
+            System.IO.File.WriteAllText(path, jsonPayLoad);
+
+
+            return Ok(newPlant);
         }
     }
 }
